05ac229156aedf5a514e2a61040876362b60ddd9
ellenlee <leechinghui.tw@gmail.com>
2016-09-05 14:40:51 +0800

    create model topic and set routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160905062431) do
+ActiveRecord::Schema.define(version: 20160905063315) do
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "nickname"
